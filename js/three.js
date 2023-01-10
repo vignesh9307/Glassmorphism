baaca7bf39b5afc4ac235b7e04834889ef1a9e65
shakthi <shaktheeswaran.ravichandran@softsuave.com>
2023-01-10 23:27:01 +0530

    cursor style

--- js/three.js
+++ js/three.js
@@ -7,6 +7,37 @@ const nebula = "./images/nebula.jpg";
 const stars = "./images/Star.png";
 const map = "./images/normalMapp.png";
 const map2 = "./images/normal-map-3.png";
+import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+
+if (window.innerWidth > 500) {
+    neonCursor({
+        el: document.getElementById("app"),
+        shaderPoints: 16,
+        curvePoints: 80,
+        curveLerp: 0.5,
+        radius1: 5,
+        radius2: 30,
+        velocityTreshold: 10,
+        sleepRadiusX: 100,
+        sleepRadiusY: 100,
+        sleepTimeCoefX: 0.0025,
+        sleepTimeCoefY: 0.0025,
+    });
+} else {
+    neonCursor({
+        el: document.getElementById("app"),
+        shaderPoints: 16,
+        curvePoints: 80,
+        curveLerp: 0.5,
+        radius1: 5,
+        radius2: 30,
+        velocityTreshold: 10,
+        sleepRadiusX: 50,
+        sleepRadiusY: 50,
+        sleepTimeCoefX: 0.0025,
+        sleepTimeCoefY: 0.0025,
+    });
+}
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true,
